14a2ea7f67e9e0a99e3824db558cf9daebf43c15
dolgoshein-na <dolgosheinna@intech.rshb.ru>
2024-03-14 16:22:46 +0300

    feat(*): add github actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), svgr(), react()],
-  base: "/",
+  base: "/farm-game/", // "/",
 });
