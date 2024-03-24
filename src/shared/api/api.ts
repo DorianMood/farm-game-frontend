13a679073e7f780a133dfe6b81ab312a12eaf507
Никита Долгошеин <nikita@Nikitas-MacBook-Pro.local>
2024-03-24 13:51:57 +0300

    feat(*): change api url

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const $api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://93.93.207.63:8080/api",
   withCredentials: true,
 });
