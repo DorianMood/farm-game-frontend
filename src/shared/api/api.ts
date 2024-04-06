a69d08f0f132612d709834304c82a5d7c4cb7b22
Никита Долгошеин <nikita@Nikitas-MacBook-Pro.local>
2024-04-06 16:04:59 +0300

    feat(api): change base url to https

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const $api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://93.93.207.63:8080/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "https://93.93.207.63:444/api",
   withCredentials: true,
 });
