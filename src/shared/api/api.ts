174ee3a52051d6007cf27efa8d2bd1576a9b097a
Никита Долгошеин <nikita@Nikitas-MacBook-Pro.local>
2024-07-03 17:34:02 +0300

    feat(api): add autologout

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,8 +1,20 @@
 import axios from "axios";
+import { RoutePath } from "shared/config/routeConfig/routeConfig";
 
 export const $api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL ?? "https://93.93.207.63:444/api",
   withCredentials: true,
 });
+
+// INFO: redicrect on 401 or other errors
+$api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = RoutePath.main;
+    }
+    return error;
+  },
+);
 // http://localhost:8000/api
 // http://2603067-tv87273.twc1.net:81/
