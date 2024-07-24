39d98a4a7716634b60ee19ed57f672ab33e5ea37
Nikita Dolgoshein <nikita01051997@gmail.com>
2024-07-24 10:54:18 +0300

    feat(*): switch to browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,24 @@
 import { createRoot } from "react-dom/client";
-import {HashRouter} from "react-router-dom";
-import { ThemeProvider } from "app/providers/ThemeProvider";
-import { StoreProvider } from "app/providers/StoreProvider";
+import {BrowserRouter} from "react-router-dom";
+import {ThemeProvider} from "app/providers/ThemeProvider";
+import {StoreProvider} from "app/providers/StoreProvider";
 import App from "./app/App";
 import "app/styles/index.scss";
-import { ErrorBoundary } from "app/providers/ErrorBoundary";
-import { AuthProvider } from "app/providers/AuthProvider/ui/AuthProvider";
+import {ErrorBoundary} from "app/providers/ErrorBoundary";
+import {AuthProvider} from "app/providers/AuthProvider/ui/AuthProvider";
 
 const container = document.getElementById("root");
 
 if (!container) {
   throw new Error(
-    "Контейнер root не найден. НЕ удалось вмонтировать реакт приложение",
+    "Контейнер root не найден. НЕ удалось вмонтировать реакт приложение"
   );
 }
 
 const root = createRoot(container);
 
 root.render(
-  <HashRouter basename={import.meta.env.BASE_URL}>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <StoreProvider>
       <ErrorBoundary>
         <ThemeProvider>
@@ -28,5 +28,5 @@ root.render(
         </ThemeProvider>
       </ErrorBoundary>
     </StoreProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
