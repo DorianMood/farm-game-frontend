facadd93be74a415a8ac93d732bc081924bf06d4
anastasiya <vladimirova-aal@rshb.intech.ru>
2024-07-31 15:15:25 +0400

    fix(task-card): add image for plant

--- src/shared/ui/TaskCard/TaskCard.tsx
+++ src/shared/ui/TaskCard/TaskCard.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import {memo} from "react";
 import CoinIcon from "shared/assets/icons/coin-16-16.svg?react";
 import cls from "./TaskCard.module.scss";
+import PlantIcon from "shared/assets/images/small-plant.png";
 
 interface TaskCardProps {
   className?: string;
@@ -24,12 +25,12 @@ export const TaskCard = memo((props: TaskCardProps) => {
       )}
     >
       {text && <p className={cls.text}>{text}</p>}
-      {coinsCount && (
-        <div className={cls.coinsCount}>
-          <CoinIcon />
-          <p className={cls.coinsCountText}>{coinsCount}</p>
-        </div>
-      )}
+        {coinsCount ? (
+            <div className={cls.coinsCount}>
+                <CoinIcon />
+                <p className={cls.coinsCountText}>{coinsCount}</p>
+            </div>
+        ) : <img src={PlantIcon} className={cls.img}/>}
     </div>
   );
 });
