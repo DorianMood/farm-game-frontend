8bf56f5bc5c6721c622fde938a318301a3513495
anastasiya <vladimirova-aal@rshb.intech.ru>
2024-07-31 15:16:50 +0400

    fix(statistic): add redirect to rating

--- src/shared/ui/StatisticsCard/StatisticsCard.tsx
+++ src/shared/ui/StatisticsCard/StatisticsCard.tsx
@@ -8,15 +8,16 @@ interface StatisticsCardProps {
   className?: string;
   cardType: StatisticsCardType;
   text?: string;
+  onClick?: () => void;
 }
 
 export const StatisticsCard = memo((props: StatisticsCardProps) => {
-  const {className, cardType, text} = props;
+  const {className, cardType, text, onClick} = props;
 
   const Icon = cardTypeIconMapper[cardType];
 
   return (
-    <div className={classNames(cls.Card, {}, [className])}>
+    <div className={classNames(cls.Card, {[cls.clicked]: !!onClick}, [className])} onClick={onClick}>
       <Icon />
       {!!text && <p className={cls.text}>{text}</p>}
     </div>
