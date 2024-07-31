fd756edbbde4f87f2ac2aa93bb272553bdad6fe9
Nikita Dolgoshein <nikita01051997@gmail.com>
2024-07-31 19:48:18 +0300

    feat(inventory): activate inventory item

--- src/json-server/api/inventory.js
+++ src/json-server/api/inventory.js
@@ -79,3 +79,6 @@ export const getInventory = () => {
   };
 };
 
+export const activateInventoryItem = () => {
+  return "OK";
+};
--- src/json-server/routes.js
+++ src/json-server/routes.js
@@ -1,114 +1,124 @@
-import {isAuthenticated, loginUser, logoutUser} from "./api/auth.js";
-import {getUser, postUser} from "./api/user.js";
-import {getInventory} from "./api/inventory.js";
-import {getBeds, postBedsHarvest, postPlant} from "./api/beds.js";
-import {getBarns, postBarnsHarvest, postStart} from "./api/barns.js";
-import {getAnimals} from "./api/animals.js";
-import {getTasks, postTask} from "./api/tasks.js";
-import {getSurveys} from "./api/surveys.js";
-import {getProducts, postPurchaseProduct, postSellProduct} from "./api/products.js";
-import {getRating} from "./api/rating.js";
+import { isAuthenticated, loginUser, logoutUser } from "./api/auth.js";
+import { getUser, postUser } from "./api/user.js";
+import { activateInventoryItem, getInventory } from "./api/inventory.js";
+import { getBeds, postBedsHarvest, postPlant } from "./api/beds.js";
+import { getBarns, postBarnsHarvest, postStart } from "./api/barns.js";
+import { getAnimals } from "./api/animals.js";
+import { getTasks, postTask } from "./api/tasks.js";
+import { getSurveys } from "./api/surveys.js";
+import {
+  getProducts,
+  postPurchaseProduct,
+  postSellProduct,
+} from "./api/products.js";
+import { getRating } from "./api/rating.js";
 
 export const routes = [
-    {
-        method: 'POST',
-        url: '/auth/login',
-        handler: loginUser
-    },
-    {
-        method: 'POST',
-        url: '/auth/logout',
-        handler: logoutUser
-    },
-    {
-        method: 'GET',
-        url: '/auth/authenticated',
-        handler: isAuthenticated
-    },
+  {
+    method: "POST",
+    url: "/auth/login",
+    handler: loginUser,
+  },
+  {
+    method: "POST",
+    url: "/auth/logout",
+    handler: logoutUser,
+  },
+  {
+    method: "GET",
+    url: "/auth/authenticated",
+    handler: isAuthenticated,
+  },
 
-    {
-        method: 'GET',
-        url: '/users',
-        handler: getUser,
-    },
-    {
-        method: 'POST',
-        url: '/users',
-        handler: postUser,
-    },
-    {
-        method: 'GET',
-        url: '/inventory',
-        handler: getInventory
-    },
-    {
-        method: 'GET',
-        url: '/beds',
-        handler: getBeds
-    },
-    {
-        method: 'POST',
-        url: '/beds/harvest',
-        handler: postBedsHarvest
-    },
-    {
-        method: 'POST',
-        url: '/beds/plant',
-        handler: postPlant
-    },
-    {
-        method: 'GET',
-        url: '/animals',
-        handler: getAnimals
-    },
-    {
-        method: 'GET',
-        url: '/barns',
-        handler: getBarns
-    },
-    {
-        method: 'POST',
-        url: '/barns/harvest',
-        handler: postBarnsHarvest
-    },
-    {
-        method: 'POST',
-        url: '/animals/start',
-        handler: postStart
-    },
-    {
-        method: 'GET',
-        url: '/tasks',
-        handler: getTasks
-    },
-    {
-        method: 'POST',
-        url: '/tasks/complete',
-        handler: postTask
-    },
-    {
-        method: 'GET',
-        url: '/surveys',
-        handler: getSurveys
-    },
-    {
-        method: 'GET',
-        url: '/products',
-        handler: getProducts
-    },
-    {
-        method: 'POST',
-        url: '/products/purchase',
-        handler: postPurchaseProduct
-    },
-    {
-        method: 'POST',
-        url: '/products/sell',
-        handler: postSellProduct
-    },
-    {
-        method: 'GET',
-        url: '/users/rating',
-        handler: getRating
-    }
-]
+  {
+    method: "GET",
+    url: "/users",
+    handler: getUser,
+  },
+  {
+    method: "POST",
+    url: "/users",
+    handler: postUser,
+  },
+  {
+    method: "GET",
+    url: "/inventory",
+    handler: getInventory,
+  },
+  {
+    method: "POST",
+    url: "/inventory/activate",
+    handler: activateInventoryItem,
+  },
+  {
+    method: "GET",
+    url: "/beds",
+    handler: getBeds,
+  },
+  {
+    method: "POST",
+    url: "/beds/harvest",
+    handler: postBedsHarvest,
+  },
+  {
+    method: "POST",
+    url: "/beds/plant",
+    handler: postPlant,
+  },
+  {
+    method: "GET",
+    url: "/animals",
+    handler: getAnimals,
+  },
+  {
+    method: "GET",
+    url: "/barns",
+    handler: getBarns,
+  },
+  {
+    method: "POST",
+    url: "/barns/harvest",
+    handler: postBarnsHarvest,
+  },
+  {
+    method: "POST",
+    url: "/animals/start",
+    handler: postStart,
+  },
+  {
+    method: "GET",
+    url: "/tasks",
+    handler: getTasks,
+  },
+  {
+    method: "POST",
+    url: "/tasks/complete",
+    handler: postTask,
+  },
+  {
+    method: "GET",
+    url: "/surveys",
+    handler: getSurveys,
+  },
+  {
+    method: "GET",
+    url: "/products",
+    handler: getProducts,
+  },
+  {
+    method: "POST",
+    url: "/products/purchase",
+    handler: postPurchaseProduct,
+  },
+  {
+    method: "POST",
+    url: "/products/sell",
+    handler: postSellProduct,
+  },
+  {
+    method: "GET",
+    url: "/users/rating",
+    handler: getRating,
+  },
+];
+
