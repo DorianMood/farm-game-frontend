11a685322057cf1cc724d05bdde6ac7b75d465f9
anastasiya <vladimirova-aal@rshb.intech.ru>
2024-09-09 17:13:58 +0400

    fix(custom-game): edit link

--- src/shared/const/games.ts
+++ src/shared/const/games.ts
@@ -1,2 +1,3 @@
 export const PLANTS_VS_ZOMBIES_ORIGIN = "https://zanael.github.io";
-export const PLANTS_VS_ZOMBIES_URL = `${PLANTS_VS_ZOMBIES_ORIGIN}/plants_vs_invaders/`;
+//export const PLANTS_VS_ZOMBIES_URL = `${PLANTS_VS_ZOMBIES_ORIGIN}/plants_vs_invaders/`;
+export const PLANTS_VS_ZOMBIES_URL = "https://plants-game.ru/";
